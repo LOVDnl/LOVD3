4f5bff8367eaa7b47001e4788e4109d3eb7404d6
Baux David <david.baux@inserm.fr>
2012-07-20 07:46:29 +0000

    Added several graphs
    git-svn-id: https://humgenprojects.lumc.nl/svn/LOVD3/branches/graphs@209 a7477d18-a37e-46de-aef1-57953c184492

--- src/status.php
+++ src/status.php
@@ -85,6 +85,18 @@ print('      <H5>Variant type (DNA level)</H5>' . "\n" .
 
 
 //begin_david
+// Variant types (DNA), whole database, pathogenic
+print('      <H5>Variant type (DNA level, pathogenic variants)</H5>' . "\n" .
+          '      <TABLE border="0" cellpadding="2" cellspacing="0" width="900" style="height : 320px;">' . "\n" .
+          '        <TR valign="top">' . "\n" .
+          '          <TD width="50%">' . "\n" .
+          '            <B>All ' . ($_AUTH['level'] >= LEVEL_COLLABORATOR? '' : 'public ') . ' variants</B><BR>'. "\n" .
+          '            <DIV id="variantsTypeDNA_all_pathogenic" style="width : 325px; height : 250px;"><IMG src="gfx/lovd_loading.gif" alt="Loading..."></DIV><BR><DIV id="variantsTypeDNA_all_pathogenic_hover">&nbsp;</DIV></TD>' . "\n" .
+          '          <TD width="50%">' . "\n" .
+          '            <B>Unique ' . ($_AUTH['level'] >= LEVEL_COLLABORATOR? '' : 'public ') . 'variants</B><BR>'. "\n" .
+          '            <DIV id="variantsTypeDNA_unique_pathogenic" style="width : 325px; height : 250px;"><IMG src="gfx/lovd_loading.gif" alt="Loading..."></DIV><BR><DIV id="variantsTypeDNA_unique_pathogenic_hover">&nbsp;</DIV></TD></TR></TABLE>' . "\n\n");
+
+
 // Screening techniques, whole database.
 print('      <H5>Screening techniques</H5>' . "\n" .
       '      <TABLE border="0" cellpadding="2" cellspacing="0" width="900" style="height : 320px;">' . "\n" .
@@ -104,20 +116,43 @@ print('      <H5>Variant type (Protein level)</H5>' . "\n" .
       '          <TD width="50%">' . "\n" .
       '            <B>Unique ' . ($_AUTH['level'] >= LEVEL_COLLABORATOR? '' : 'public ') . 'variants</B><BR>'. "\n" .
       '            <DIV id="variantsTypeProtein_unique" style="width : 325px; height : 250px;"><IMG src="gfx/lovd_loading.gif" alt="Loading..."></DIV><DIV id="variantsTypeProtein_unique_hover">&nbsp;</DIV></TD></TR></TABLE>' . "\n\n");
+// Variant types (protein level), whole database, pathogenic.
+print('      <H5>Variant type (Protein level, pathogenic variants)</H5>' . "\n" .
+      '      <TABLE border="0" cellpadding="2" cellspacing="0" width="900" style="height : 320px;">' . "\n" .
+      '        <TR valign="top">' . "\n" .
+      '          <TD width="50%">' . "\n" .
+      '            <B>All ' . ($_AUTH['level'] >= LEVEL_COLLABORATOR? '' : 'public ') . ' variants</B><BR>'. "\n" .
+      '            <DIV id="variantsTypeProtein_all_pathogenic" style="width : 325px; height : 250px;"><IMG src="gfx/lovd_loading.gif" alt="Loading..."></DIV><DIV id="variantsTypeProtein_all_pathogenic_hover">&nbsp;</DIV></TD>' . "\n" .
+      '          <TD width="50%">' . "\n" .
+      '            <B>Unique ' . ($_AUTH['level'] >= LEVEL_COLLABORATOR? '' : 'public ') . 'variants</B><BR>'. "\n" .
+      '            <DIV id="variantsTypeProtein_unique_pathogenic" style="width : 325px; height : 250px;"><IMG src="gfx/lovd_loading.gif" alt="Loading..."></DIV><DIV id="variantsTypeProtein_unique_pathogenic_hover">&nbsp;</DIV></TD></TR></TABLE>' . "\n\n");
+// Variant positions, whole database.
+    print('      <H5>Variant positions</H5>' . "\n" .
+      '      <TABLE border="0" cellpadding="2" cellspacing="0" width="900" style="height : 320px;">' . "\n" .
+      '        <TR valign="top">' . "\n" .
+      '          <TD width="50%">' . "\n" .
+      '            <B>Unique ' . ($_AUTH['level'] >= LEVEL_COLLABORATOR? '' : 'public ') . ' variants</B><BR>'. "\n" .
+      '            <DIV id="variantsPositions" style="width : 325px; height : 250px;"><IMG src="gfx/lovd_loading.gif" alt="Loading..."></DIV><DIV id="variantsPositions_hover">&nbsp;</DIV></TD>' . "\n" .
+      '         </TR></TABLE>' . "\n\n");
 //end_david
 
 flush();
 @ob_end_flush(); // Can generate errors on the screen if no buffer found.
 $_G->genesNumberOfVariants('genesNumberOfVariants', '*', ($_AUTH['level'] >= LEVEL_COLLABORATOR));
 $_G->genesLinkedDiseases('genesLinkedDiseases', '*');
-$_G->variantsTypeDNA('variantsTypeDNA_all', '*', ($_AUTH['level'] >= LEVEL_COLLABORATOR), false);
-$_G->variantsTypeDNA('variantsTypeDNA_unique', '*', ($_AUTH['level'] >= LEVEL_COLLABORATOR), true);
+$_G->variantsTypeDNA('variantsTypeDNA_all', '*', ($_AUTH['level'] >= LEVEL_COLLABORATOR), false, false);
+$_G->variantsTypeDNA('variantsTypeDNA_unique', '*', ($_AUTH['level'] >= LEVEL_COLLABORATOR), true, false);
 
 //begin_david
 //screening techniques
+$_G->variantsTypeDNA2('variantsTypeDNA_all_pathogenic', '*', ($_AUTH['level'] >= LEVEL_COLLABORATOR), false, true);
+$_G->variantsTypeDNA2('variantsTypeDNA_unique_pathogenic', '*', ($_AUTH['level'] >= LEVEL_COLLABORATOR), true, true);
 $_G->screeningTechniques('screeningTechniques', '*', ($_AUTH['level'] >= LEVEL_COLLABORATOR), true);
-$_G->variantsTypeProtein('variantsTypeProtein_all', '*', ($_AUTH['level'] >= LEVEL_COLLABORATOR), false);
-$_G->variantsTypeProtein('variantsTypeProtein_unique', '*', ($_AUTH['level'] >= LEVEL_COLLABORATOR), true);
+$_G->variantsTypeProtein('variantsTypeProtein_all', '*', ($_AUTH['level'] >= LEVEL_COLLABORATOR), false, false);
+$_G->variantsTypeProtein('variantsTypeProtein_unique', '*', ($_AUTH['level'] >= LEVEL_COLLABORATOR), true, false);
+$_G->variantsTypeProtein('variantsTypeProtein_all_pathogenic', '*', ($_AUTH['level'] >= LEVEL_COLLABORATOR), false, true);
+$_G->variantsTypeProtein('variantsTypeProtein_unique_pathogenic', '*', ($_AUTH['level'] >= LEVEL_COLLABORATOR), true, true);
+$_G->variantsPositions('variantsPositions', '*', ($_AUTH['level'] >= LEVEL_COLLABORATOR));
 //end_david
 
 
