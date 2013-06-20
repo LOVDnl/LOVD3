582be9e751a94942d24abb25f64af02e20ee7fbc
Ivo Fokkema <I.F.A.C.Fokkema@lumc.nl>
2013-06-20 13:22:31 +0000

    Some work on the manual: - Fixed all underfull and overfull hboxes in the LaTeX logs. - Fixed a typo. - Added information on how to reset shared columns when editing the system-wide settings.
    git-svn-id: https://humgenprojects.lumc.nl/svn/LOVD3/trunk@310 a7477d18-a37e-46de-aef1-57953c184492

--- src/docs/index.php
+++ src/docs/index.php
@@ -4,8 +4,8 @@
  * LEIDEN OPEN VARIATION DATABASE (LOVD)
  *
  * Created     : 2012-11-27
- * Modified    : 2013-04-18
- * For LOVD    : 3.0-04
+ * Modified    : 2013-06-20
+ * For LOVD    : 3.0-06
  *
  * Copyright   : 2004-2013 Leiden University Medical Center; http://www.LUMC.nl/
  * Programmer  : Ing. Ivo F.A.C. Fokkema <I.F.A.C.Fokkema@LUMC.nl>
@@ -46,7 +46,7 @@ if (PATH_COUNT == 1 && !ACTION) {
 
     print('      The LOVD 3.0 documentation is currently still work in progress.<BR>Currently available is the LOVD 3.0 user manual.<BR>' .
           '      <UL>' . "\n" .
-          '        <LI><A href="docs/LOVD_manual_3.0.pdf" target="_blank">LOVD manual 3.0-04</A> (38 pages, 919Kb) - last updated April 16th 2013</LI></UL>' . "\n\n");
+          '        <LI><A href="docs/LOVD_manual_3.0.pdf" target="_blank">LOVD manual 3.0-06</A> (39 pages, 919Kb) - last updated June 20th 2013</LI></UL>' . "\n\n");
 
     $_T->printFooter();
     exit;
