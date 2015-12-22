194fd9ca49de05ae7c7a74e37482edd907205693
Ivo Fokkema <I.F.A.C.Fokkema@lumc.nl>
2015-12-22 17:09:56 +0000

    Fixed notices in inc-lib-viewlist.php caused by search terms with surrounding spaces or more than one space between two search terms.
    git-svn-id: https://humgenprojects.lumc.nl/svn/LOVD3/trunk@713 a7477d18-a37e-46de-aef1-57953c184492

--- src/inc-lib-viewlist.php
+++ src/inc-lib-viewlist.php
@@ -4,10 +4,10 @@
  * LEIDEN OPEN VARIATION DATABASE (LOVD)
  *
  * Created     : 2010-01-22
- * Modified    : 2013-07-03
- * For LOVD    : 3.0-07
+ * Modified    : 2015-12-22
+ * For LOVD    : 3.0-15
  *
- * Copyright   : 2004-2013 Leiden University Medical Center; http://www.LUMC.nl/
+ * Copyright   : 2004-2015 Leiden University Medical Center; http://www.LUMC.nl/
  * Programmers : Ing. Ivo F.A.C. Fokkema <I.F.A.C.Fokkema@LUMC.nl>
  *               Ing. Ivar C. Lugtenburg <I.C.Lugtenburg@LUMC.nl>
  *
@@ -48,14 +48,18 @@ function lovd_formatSearchExpression ($sExpression, $sColumnType)
 {
     // Formats the search expressions for the "title" text of the input field so that the users can understand what their search does.
     if ($sColumnType == 'DATETIME') {
-        $sExpression = preg_replace('/ (\d)/', "{{SPACE}}$1", $sExpression);
+        $sExpression = preg_replace('/ (\d)/', "{{SPACE}}$1", trim($sExpression));
     } else {
-        $sExpression = preg_replace_callback('/("[^"]+")/', create_function('$aRegs', 'return str_replace(\' \', \'{{SPACE}}\', $aRegs[1]);'), $sExpression);
+        $sExpression = preg_replace_callback('/("[^"]+")/', create_function('$aRegs', 'return str_replace(\' \', \'{{SPACE}}\', $aRegs[1]);'), trim($sExpression));
     }
     $aANDExpressions = explode(' ', $sExpression);
     $nANDLength = count($aANDExpressions);
     $sFormattedExpressions = '';
     foreach ($aANDExpressions as $nANDIndex => $sANDExpression) {
+        if (!trim($sANDExpression)) {
+            // Double spaces in the search terms.
+            continue;
+        }
         $aORExpressions = explode('|', $sANDExpression);
         $nORLength = count($aORExpressions);
         $sFormattedExpression = ($sColumnType == 'TEXT'? ' - ' : ' ');
