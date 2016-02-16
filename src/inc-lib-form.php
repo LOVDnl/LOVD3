9d44023d43c16fe68a2306cb5cb04f517fea8c56
M. Kroon <m.kroon@lumc.nl>
2016-02-16 12:58:44 +0100

    Have input forms optionally try to block autofill username/password.
    Makes the password fields read-only until it captures focus. This
    prevents browsers from filling username and passwords automatically.
    Tested on Firefox 44 and Chromium 48.

--- src/inc-lib-form.php
+++ src/inc-lib-form.php
@@ -752,7 +752,8 @@ function lovd_viewForm ($a,
                         $sHelpSuffix   = '</TD>',
                         $sDataPrefix   = "\n            <TD class=\"{{ CLASS }}\">",
                         $sDataSuffix   = '</TD></TR>',
-                        $sNewLine      = '              ')
+                        $sNewLine      = '              ',
+                        $enableAutofillCred = false)
 {
     // Based on a function provided by Ileos.nl.
     /***************************************************************************
@@ -775,6 +776,8 @@ function lovd_viewForm ($a,
      * array('<header>', '<help_text>', 'checkbox', '<field_name>'),
      * array('<header>', '<help_text>', 'submit', '<button_value>', '<field_name>'),
      *
+     * If parameter $enableAutofillCred is false, the function will try to prevent
+     * the browser from automatically filling credential fields (username/password)
      **********/
 
     // Options.
@@ -894,7 +897,12 @@ function lovd_viewForm ($a,
                     $GLOBALS['_' . $sMethod][$sName] = '';
                 }
 
-                print('<INPUT type="' . $sType . '" name="' . $sName . '" size="' . $nSize . '" value="' . htmlspecialchars($GLOBALS['_' . $sMethod][$sName]) . '"' . (!lovd_errorFindField($sName) ? '' : ' class="err"') . '>' . $sDataSuffix);
+                $autofillBlockerAtts = '';
+                if ($sType == 'password' && !$enableAutofillCred) {
+                    // Block editing of the actual password field until onFocus event.
+                    $autofillBlockerAtts = ' readonly onfocus="this.removeAttribute(\'readonly\');" ';
+                }
+                print('<INPUT type="' . $sType . '" name="' . $sName . '" size="' . $nSize . '" value="' . htmlspecialchars($GLOBALS['_' . $sMethod][$sName]) . '"' . (!lovd_errorFindField($sName)? '' : ' class="err"') . $autofillBlockerAtts . '>' . $sDataSuffix);
                 continue;
 
 
