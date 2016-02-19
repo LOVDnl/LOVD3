75a9370b57266139d7d8111eac8d7bfe353ff3b9
Ivo F.A.C. Fokkema <i.f.a.c.fokkema@lumc.nl>
2016-02-19 14:22:33 +1100

    Fixed integration tests after minor change to output of authorization unit test.

--- tests/selenium_tests/authorization_tests/authorization.php
+++ tests/selenium_tests/authorization_tests/authorization.php
@@ -10,7 +10,7 @@ class Example extends PHPUnit_Extensions_SeleniumTestCase
   public function testMyTestCase()
   {
     $this->open("/svn/LOVD3/trunk/tests/unit_tests/authorization.php");
-    $this->assertEquals("Complete, all successful", $this->getText("css=pre"));
+    $this->assertEquals("Complete, all successful.", $this->getText("css=pre"));
   }
 }
 ?>
