7c6a85acb21b1527e7bbca62fb752d58706565b5
ifacfokkema <i.f.a.c.fokkema@lumc.nl>
2016-05-18 18:15:34 +0200

    Fixed failing tests after the most recent commit introduced textual changes.

--- tests/selenium_tests/collaborator_tests/add_summary_variant_vcf_file.php
+++ tests/selenium_tests/collaborator_tests/add_summary_variant_vcf_file.php
@@ -6,6 +6,6 @@ class AddSummaryVariantVCFFileTest extends LOVDSeleniumBaseTestCase
     public function testAddSummaryVariantVCFFile()
     {
         $this->open(ROOT_URL . "/src/variants/upload?create&type=VCF");
-        $this->assertEquals("To access this area, you need at least Submitter (data owner) clearance.", $this->getText("css=table[class=info]"));
+        $this->assertEquals("To access this area, you need at least Curator clearance.", $this->getText("css=table[class=info]"));
     }
 }
--- tests/selenium_tests/submitter_tests/add_summary_variant_only_described_on_genomic_level.php
+++ tests/selenium_tests/submitter_tests/add_summary_variant_only_described_on_genomic_level.php
@@ -6,6 +6,6 @@ class AddSummaryVariantOnlyDescribedOnGenomicLevelTest extends LOVDSeleniumBaseT
     public function testAddSummaryVariantOnlyDescribedOnGenomicLevel()
     {
         $this->open(ROOT_URL . "/src/variants?create&reference=Genome");
-        $this->assertEquals("To access this area, you need at least Submitter (data owner) clearance.", $this->getText("css=table[class=info]"));
+        $this->assertEquals("To access this area, you need at least Curator clearance.", $this->getText("css=table[class=info]"));
     }
 }
