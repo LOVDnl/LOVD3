LOVDnl/LOVD3

commit 358524ffb4db8007c2076b06a2d013c2bd265888

Use lovd_trim() in LOVD2 conversion.
* Moved lovd_trim() from import.php to inc-lib-form.php.
* Call lovd_trim() in convert_lovd2.php for every input field.
* Removed lovd_empty(), as it's now obsolete.
* Added quotation marks around all output fields in convert_lovd2.php

src/scripts/convert_lovd2.php
@@ -377,19 +377,6 @@ function lovd_convertUserID ($nLOVD2UserID, $sType = 'curator')
 
 
 
-function lovd_empty ($sValue)
-{
-    // Returns true if given LOVD2 input field value ($sValue) is considered
-    // empty (i.e. an empty string, null or a string with just opening and
-    // closing quotation marks.
-
-    return (is_null($sValue) || in_array($sValue, array('', '""', "''")));
-}
-
-
-
-
-
 function lovd_getDiseaseID ($sDiseaseName)
 {
     // Get the ID from the database, searching the name and symbol fields for
@@ -659,7 +646,7 @@ function lovd_getRecordForHeaders ($aOutputHeaders, $aRecord, $aSection = null)
         }
         if (isset($aSection['mandatory_fields']) &&
             isset($aSection['mandatory_fields'][$sHeader]) &&
-            lovd_empty($aNewRecord[$sHeader])) {
+            empty($aNewRecord[$sHeader])) {
             // Set default value for mandatory field.
             $aNewRecord[$sHeader] = $aSection['mandatory_fields'][$sHeader];
         }
@@ -689,14 +676,14 @@ function lovd_getSectionOutput ($aImportSection, $aOutputHeaders, $aRecords)
     // will be duplicated when multiple input fields link to the same output.
     $aUniqueHeaders = array_unique($aOutputHeaders);
     $sOutput .= implode("\t", array_map(function ($sHeader) {
-        return '{{' . $sHeader . '}}';
+        return '{{"' . $sHeader . '"}}';
     }, $aUniqueHeaders)) . "\n";
 
     foreach ($aRecords as $aRecord) {
         // Put record fields in same order as headers.
         $aOutRecord = array();
         foreach ($aUniqueHeaders as $sHeader) {
-            $aOutRecord[] = $aRecord[$sHeader];
+            $aOutRecord[] = '"' . $aRecord[$sHeader] . '"';
         }
         $sOutput .= implode("\t", $aOutRecord) . "\n";
     }
@@ -814,7 +801,7 @@ function lovd_parseData ($aData, $zTranscript, $aFieldLinks, $aInputHeaders, $aO
         $aRecord = array();
         for ($i = 0; $i < count($aInputRecord); $i++) {
             $sFieldName = $aInputHeaders[$i];
-            $sFieldValue = $aInputRecord[$i];
+            $sFieldValue = lovd_trim($aInputRecord[$i]);
             if (isset($aFieldLinks[$sFieldName])) {
                 if (count($aFieldLinks[$sFieldName]) == 2) {
                     $aRecord[] = $sFieldValue;
@@ -851,7 +838,7 @@ function lovd_parseData ($aData, $zTranscript, $aFieldLinks, $aInputHeaders, $aO
         // Create new disease if necessary.
         $aDisease = lovd_getRecordForHeaders($aOutputHeaders['disease'], $aRecord,
             $aSections['disease']);
-        if (!lovd_empty($aDisease['name'])) {
+        if (!empty($aDisease['name'])) {
             list($aDisease['id'], $bCreateNewDisease) = lovd_getDiseaseID($aDisease['name']);
             if ($aDisease['id'] === false) {
                 // Something went wrong determining the disease, stop parsing.
@@ -899,7 +886,7 @@ function lovd_parseData ($aData, $zTranscript, $aFieldLinks, $aInputHeaders, $aO
                 $bEmptyPheno = true;
                 foreach ($aPhenotype as $k => $v) {
                     $bEmptyPheno = $bEmptyPheno &&
-                        (in_array($k, array('id', 'diseaseid', 'individualid')) || lovd_empty($v));
+                        (in_array($k, array('id', 'diseaseid', 'individualid')) || empty($v));
                 }
                 if (!$bEmptyPheno) {
                     // Skip phenotype because there is no data in phenotype
@@ -1011,9 +998,6 @@ function lovd_setUserIDSettings ($sFixedSubmitterIDInput, $sSubmitterTranslation
     //                              ID value to translate, the second int is
     //                              the LOVD3 user ID value to which is being
     //                              translated.
-
-    global $sFixedSubmitterID, $aSubmitterTranslationTable, $sFixedCuratorID,
-           $aCuratorTranslationTable;
 
     $aFixedUserInfos = array(
         array('submitter', 'submitterid_fixed', 'sFixedSubmitterID', $sFixedSubmitterIDInput),

src/import.php
@@ -4,8 +4,8 @@
  * LEIDEN OPEN VARIATION DATABASE (LOVD)
  *
  * Created     : 2012-09-19
- * Modified    : 2016-11-15
- * For LOVD    : 3.0-17
+ * Modified    : 2016-12-12
+ * For LOVD    : 3.0-18
  *
  * Copyright   : 2004-2016 Leiden University Medical Center; http://www.LUMC.nl/
  * Programmers : Ing. Ivo F.A.C. Fokkema <I.F.A.C.Fokkema@LUMC.nl>
@@ -215,22 +215,6 @@ function lovd_setEmptyCheckboxFields ($aForm)
             }
         }
     }
-}
-
-
-
-
-
-function lovd_trimField ($sVal)
-{
-    // Trims data fields in an intelligent way. We don't just strip the quotes off, as this may effect quotes in the fields.
-    // Instead, we check if the field is surrounded by quotes. If so, we take the first and last character off and return the field.
-
-    $sVal = trim($sVal);
-    if ($sVal && $sVal{0} == '"' && substr($sVal, -1) == '"') {
-        $sVal = substr($sVal, 1, -1); // Just trim the first and last quote off, nothing else!
-    }
-    return trim($sVal);
 }
 
 

src/inc-lib-form.php
@@ -4,8 +4,8 @@
  * LEIDEN OPEN VARIATION DATABASE (LOVD)
  *
  * Created     : 2009-10-21
- * Modified    : 2016-08-26
- * For LOVD    : 3.0-17
+ * Modified    : 2016-12-12
+ * For LOVD    : 3.0-18
  *
  * Copyright   : 2004-2016 Leiden University Medical Center; http://www.LUMC.nl/
  * Programmers : Ing. Ivo F.A.C. Fokkema <I.F.A.C.Fokkema@LUMC.nl>
@@ -739,6 +739,22 @@ function lovd_setUpdatedDate ($aGenes)
     // Just update the database and we'll see what happens.
     $q = $_DB->query('UPDATE ' . TABLE_GENES . ' SET updated_by = ?, updated_date = NOW() WHERE id IN (?' . str_repeat(', ?', count($aGenes) - 1) . ')', array_merge(array($_AUTH['id']), $aGenes), false);
     return ($q->rowCount());
+}
+
+
+
+
+
+function lovd_trimField ($sVal)
+{
+    // Trims data fields in an intelligent way. We don't just strip the quotes off, as this may effect quotes in the fields.
+    // Instead, we check if the field is surrounded by quotes. If so, we take the first and last character off and return the field.
+
+    $sVal = trim($sVal);
+    if ($sVal && $sVal{0} == '"' && substr($sVal, -1) == '"') {
+        $sVal = substr($sVal, 1, -1); // Just trim the first and last quote off, nothing else!
+    }
+    return trim($sVal);
 }
 
 

commit d1a1a66edb329c7136f5c5308c35c3c6455fb09d

Bugfix: replaced lovd_trim() calls with lovd_trimField() in lovd2 conv.
* Bug was introduced in 358524f

src/scripts/convert_lovd2.php
@@ -235,7 +235,7 @@ function lovd_convertDBID ($sLOVD2DBID)
     // Returns an LOVD3-formatted DBID for the given $sLOVD2DBID by padding
     // the number with an extra '0'.
 
-    $aChunks = explode('_', lovd_trim($sLOVD2DBID));
+    $aChunks = explode('_', $sLOVD2DBID);
     $nParts = count($aChunks);
     if ($nParts > 1 && ctype_digit($aChunks[$nParts-1])) {
         $aChunks[$nParts-1] = '0' . $aChunks[$nParts-1];
@@ -342,7 +342,7 @@ function lovd_convertUserID ($nLOVD2UserID, $sType = 'curator')
     global $sFixedSubmitterID, $aSubmitterTranslationTable, $sFixedCuratorID,
            $aCuratorTranslationTable, $_WARNINGS;
 
-    $LOVD2UserIDClean = intval(lovd_trim($nLOVD2UserID));
+    $LOVD2UserIDClean = intval($nLOVD2UserID);
     if ($sType == 'curator') {
         // Convert curator ID.
         if ($LOVD2UserIDClean === 0) {
@@ -393,27 +393,26 @@ function lovd_getDiseaseID ($sDiseaseName)
     static $aKnownDiseases;
 
     $bNewDisease = false;
-    $sDiseaseNameClean = lovd_trim($sDiseaseName);
-    if (!isset($aKnownDiseases[$sDiseaseNameClean])) {
+    if (!isset($aKnownDiseases[$sDiseaseName])) {
         $zDiseases = $_DB->query('SELECT id FROM ' . TABLE_DISEASES . ' WHERE name = ? OR symbol = ?',
-            array($sDiseaseNameClean, $sDiseaseNameClean));
+            array($sDiseaseName, $sDiseaseName));
         $aDiseases = $zDiseases->fetchAllAssoc();
         if (!$aDiseases) {
             // Not in database: create new unique disease ID.
-            $aKnownDiseases[$sDiseaseNameClean] = lovd_getInc('Diseases');
+            $aKnownDiseases[$sDiseaseName] = lovd_getInc('Diseases');
             $bNewDisease = true;
         } elseif (count($aDiseases) > 1) {
             // Multiple hits in database.
-            lovd_errorAdd('LOVD2_export', 'Error: disease name "' . $sDiseaseNameClean .
+            lovd_errorAdd('LOVD2_export', 'Error: disease name "' . $sDiseaseName .
                 '" is ambiguous, it matches name or symbol for more than one disease in the ' .
                 'database.');
             return array(false, false);
         } else {
             // Exactly one hit in database.
-            $aKnownDiseases[$sDiseaseNameClean] = $aDiseases[0]['id'];
+            $aKnownDiseases[$sDiseaseName] = $aDiseases[0]['id'];
         }
     }
-    return array($aKnownDiseases[$sDiseaseNameClean], $bNewDisease);
+    return array($aKnownDiseases[$sDiseaseName], $bNewDisease);
 }
 
 
@@ -803,7 +802,7 @@ function lovd_parseData ($aData, $zTranscript, $aFieldLinks, $aInputHeaders, $aO
         $aRecord = array();
         for ($i = 0; $i < count($aInputRecord); $i++) {
             $sFieldName = $aInputHeaders[$i];
-            $sFieldValue = lovd_trim($aInputRecord[$i]);
+            $sFieldValue = lovd_trimField($aInputRecord[$i]);
             if (isset($aFieldLinks[$sFieldName])) {
                 if (count($aFieldLinks[$sFieldName]) == 2) {
                     $aRecord[] = $sFieldValue;
@@ -935,7 +934,7 @@ function lovd_parseData ($aData, $zTranscript, $aFieldLinks, $aInputHeaders, $aO
 
         // Get positions on transcript/chromosome from mutalyzer for variant.
         $nHGVSIdx = array_search('Variant/DNA', $aInputHeaders);
-        $sVariant = lovd_trim($aRecord[$nHGVSIdx]);
+        $sVariant = $aRecord[$nHGVSIdx];
         $aMappingInfoArgs = array(
             'LOVD_ver' => $_SETT['system']['version'],
             'build' => 'hg19',
@@ -1128,14 +1127,6 @@ function lovd_showConversionForm ($nMaxSizeLOVD, $nMaxSize)
 
 
 
-function lovd_trim ($sValue)
-{
-    return trim($sValue, '"\' ');
-}
-
-
-
-
 
 function lovd_validateConversionForm ($zTranscript, $nMaxSize, $nMaxSizeLOVD)
 {
@@ -1269,7 +1260,7 @@ function main ($aFieldLinks, $aSections, $aCustomColLinks)
             <TEXTAREA id="conversion_output" cols="100" rows="20" style="font-family: monospace; 
         white-space: nowrap; overflow: scroll;">' .
             $sOutput .
-            '</TEXTAREA>
+            '</TEXTAREA><BR>
             <BUTTON id="copybutton">Copy content to clipboard</BUTTON>
             <SCRIPT language="JavaScript">
                 $("#copybutton").on("click", function () {