LOVDnl/LOVD3

commit b794cfb467991acac75940553523765c6347fa1d

Fix for links falling off page header with long gene names.

src/class/template.php
@@ -641,7 +641,7 @@ foreach ($zAnnouncements as $zAnnouncement) {
               '      <IMG src="' . $_CONF['logo_uri'] . '" alt="LOVD - Leiden Open Variation Database" ' . $sSize . '>' . "\n" .
               '    </TD>' . "\n");
 
-        print('    <TD valign="top" style="padding-top : 2px; white-space : nowrap; width : 100%">' . "\n" .
+        print('    <TD valign="top" style="padding-top : 2px; width : 100%">' . "\n" .
               '      <H2 style="margin-bottom : 2px;">' . $_CONF['system_title'] . '</H2>');
 
         if ($sCurrSymbol && $sCurrGene) {
@@ -817,13 +817,14 @@ foreach ($zAnnouncements as $zAnnouncement) {
               '</TABLE>' . "\n\n" .
               '<IMG src="gfx/trans.png" alt="" width="' . $nTotalTabWidth . '" height="0">' . "\n\n");
 
-        if (!empty($zAnnouncements)) {
-            // Measure the height of the sticky header (can depend on announcements or
-            // font settings and such), and adapt the menu table to have a margin of this height.
-            print('<SCRIPT type="text/javascript">' . "\n" .
-                  '  $("table.logo :eq(1)").css("margin-top", $("#stickyheader").outerHeight(true) + "px");' . "\n" .
-                  '</SCRIPT>' . "\n\n");
-        }
+        // Measure the height of the sticky header (can depend on announcements or
+        // font settings and such), and adapt the menu table to have a margin of this height.
+        // Note: we're not using $(document).ready() here to keep the user experience as
+        // smooth as possible (e.g. no visible movement of the logo table). This means that
+        // the javascript below must come AFTER the #stickyheader element is printed.
+        print('<SCRIPT type="text/javascript">' . "\n" .
+              '  $("table.logo :eq(1)").css("margin-top", $("#stickyheader").outerHeight(true) + "px");' . "\n" .
+              '</SCRIPT>' . "\n\n");
 
         // Attach dropdown menus.
         print('<!-- Start drop down menu definitions -->' . "\n");

commit b8c3b2804b8af611261651e9de659584107b8004

Shorten gene name in sticky header.
* Reversed commit b794cfb which introduced a problem when the sticky
  header was enlarged by the gene name and one clicked the gene
  switcher. That would decrease the header size and introduce a gap
  between the header and rest of the lay out.

src/class/template.php
@@ -641,11 +641,12 @@ foreach ($zAnnouncements as $zAnnouncement) {
               '      <IMG src="' . $_CONF['logo_uri'] . '" alt="LOVD - Leiden Open Variation Database" ' . $sSize . '>' . "\n" .
               '    </TD>' . "\n");
 
-        print('    <TD valign="top" style="padding-top : 2px; width : 100%">' . "\n" .
+        print('    <TD valign="top" style="padding-top : 2px; white-space : nowrap; width : 100%">' . "\n" .
               '      <H2 style="margin-bottom : 2px;">' . $_CONF['system_title'] . '</H2>');
 
         if ($sCurrSymbol && $sCurrGene) {
-            print('      <H5 id="gene_name" style="display:inline">' . $sCurrGene . ' (' . $sCurrSymbol . ')' . "\n");
+            $sGeneName = lovd_shortenString($sCurrSymbol . ' (' . $sCurrGene . ')', 50);
+            print('      <H5 id="gene_name" style="display:inline">' . $sGeneName . "\n");
             if (strpos($sGeneSwitchURL, '{{GENE}}') !== false) {
                 print('        <A href="#" onclick="lovd_switchGene(); return false;">' . "\n" .
                       '          <IMG src="gfx/lovd_genes_switch_inline.png" width="23" height="23" alt="Switch gene" title="Switch gene database" align="top">' . "\n" .
@@ -822,9 +823,11 @@ foreach ($zAnnouncements as $zAnnouncement) {
         // Note: we're not using $(document).ready() here to keep the user experience as
         // smooth as possible (e.g. no visible movement of the logo table). This means that
         // the javascript below must come AFTER the #stickyheader element is printed.
-        print('<SCRIPT type="text/javascript">' . "\n" .
-              '  $("table.logo :eq(1)").css("margin-top", $("#stickyheader").outerHeight(true) + "px");' . "\n" .
-              '</SCRIPT>' . "\n\n");
+        if (!empty($zAnnouncements)) {
+            print('<SCRIPT type="text/javascript">' . "\n" .
+                  '  $("table.logo :eq(1)").css("margin-top", $("#stickyheader").outerHeight(true) + "px");' . "\n" .
+                  '</SCRIPT>' . "\n\n");
+        }
 
         // Attach dropdown menus.
         print('<!-- Start drop down menu definitions -->' . "\n");