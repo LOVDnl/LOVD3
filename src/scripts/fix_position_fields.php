a2a3e22ed0f8755c3812a86d0b5c298704f509c7
Ivo Fokkema <i.f.a.c.fokkema@lumc.nl>
2017-01-28 18:06:40 +1100

    Added script that analyses variant position problems and will fix them. - Currently, only the framework has been written. - It analyses some of the possible errors, and reports them. - When you press run, it will fetch the problematic data, run a (stub) function over the data, and updates the table and progress bar. - The functions that fix the data, and some of the queries that fetch the problematic data, still need to be written. - Updated styles.css to allow simple highlighting of a row in an data table.

---
+++ src/scripts/fix_position_fields.php
@@ -0,0 +1,281 @@
+<?php
+/*******************************************************************************
+ *
+ * LEIDEN OPEN VARIATION DATABASE (LOVD)
+ *
+ * Created     : 2017-01-28
+ * Modified    : 2017-01-28
+ * For LOVD    : 3.0-19
+ *
+ * Copyright   : 2004-2017 Leiden University Medical Center; http://www.LUMC.nl/
+ * Programmer  : Ivo F.A.C. Fokkema <I.F.A.C.Fokkema@LUMC.nl>
+ *
+ *
+ * This file is part of LOVD.
+ *
+ * LOVD is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LOVD is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LOVD.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *************/
+
+define('ROOT_PATH', '../');
+require ROOT_PATH . 'inc-init.php';
+
+// But we don't care about your session (in fact, it locks the whole LOVD if we keep this page running).
+session_write_close();
+
+define('PAGE_TITLE', 'Fix variant position fields');
+$_T->printHeader();
+$_T->printTitle();
+lovd_showInfoTable('This script is designed to verify and check all variant position fields in the database.
+    The variant position fields may be wrong because of mistakes made in the import file when importing variants.
+    Also, variants may not have been understood by LOVD before and therefore may be lacking positions.
+    If you only manually added variants to LOVD, and adhered to the HGVS nomenclature standards, this script will not be useful.');
+
+lovd_requireAUTH(LEVEL_MANAGER);
+
+
+
+
+
+// Class for the Analyses.
+class LOVD_VariantPositionAnalyses {
+    protected $aAnalyses = array();
+    protected $oBAR = NULL;
+
+
+
+
+
+    public function __construct()
+    {
+        // Constructor that will load the progress bar and set up the analyses.
+        global $_DB;
+
+        // Set up the progress bar that we'll be reusing all the time.
+        require_once ROOT_PATH . 'class/progress_bar.php';
+        $_BAR = new ProgressBar();
+        $_BAR->setMessageVisibility('done', true);
+        $this->oBAR =& $_BAR; // We need it reachable by other methods, but static functions don't like member variables.
+
+        // Define analyses.
+        $this->aAnalyses = array(
+            'vog_total_variants' => // Total number of variants in the database.
+                array(
+                    'sql_count' => 'SELECT COUNT(*) FROM ' . TABLE_VARIANTS,
+                ),
+            'vog_positions_swapped' => // The number of positions that have been swapped (end > start).
+                array(
+                    'sql_count' => 'SELECT COUNT(*) FROM ' . TABLE_VARIANTS . ' WHERE position_g_start > position_g_end',
+                    'sql_fetch' => 'SELECT id, position_g_start, position_g_end FROM ' . TABLE_VARIANTS . ' WHERE position_g_start > position_g_end ORDER BY chromosome, position_g_start, position_g_end',
+                    'fix' => function ($zRow) use ($_DB)
+                    {
+                        // Function body here.
+                        return 0;
+                    },
+                ),
+            'vog_positions_in_error' => // The number of positions that do not match the variant's positions (analysis needed).
+                array(
+                    'fix' => function ($zRow) use ($_DB)
+                    {
+                        // Function body here.
+                        return 0;
+                    },
+                ),
+            'vog_positions_missing' => // The number of variants that have no position fields.
+                array(
+                    'sql_count' => 'SELECT COUNT(*) FROM ' . TABLE_VARIANTS . ' WHERE position_g_start IS NULL OR position_g_start = 0 OR position_g_end IS NULL OR position_g_end = 0',
+                    'sql_fetch' => 'SELECT id, position_g_start, position_g_end, `VariantOnGenome/DNA` FROM ' . TABLE_VARIANTS . ' WHERE position_g_start IS NULL OR position_g_start = 0 OR position_g_end IS NULL OR position_g_end = 0 ORDER BY chromosome, position_g_start, position_g_end',
+                    'fix' => function ($zRow) use ($_DB)
+                    {
+                        // Function body here.
+                        return 0;
+                    },
+                ),
+            'vog_variants_not_understood' => // The number of variants that have no position fields and can't be recognized by LOVD (analysis needed).
+                array(
+                    'fix' => function ($zRow) use ($_DB)
+                    {
+                        // Function body here.
+                        return 0;
+                    },
+                ),
+        );
+
+        // Start counting already, because we always need these numbers.
+        $this->oBAR->setMessage('Analysing...');
+        $this->oBAR->setProgress(0);
+        foreach ($this->aAnalyses as $sAnalysis => $aAnalysis) {
+            if (isset($aAnalysis['sql_count'])) {
+                $this->aAnalyses[$sAnalysis]['count'] = $_DB->query($aAnalysis['sql_count'])->fetchColumn();
+            }
+        }
+        $this->oBAR->setMessage('<BR>');
+    }
+
+
+
+
+
+    public function runAnalyses ()
+    {
+        // Runs the analyses, updates the stats that should have been printed already, and runs the fixes.
+        global $_DB;
+
+        $this->oBAR->setProgress(0);
+        foreach ($this->aAnalyses as $sAnalysis => $aAnalysis) {
+            // Mark this row as being active.
+            print('
+      <SCRIPT type="text/javascript">$("#analyses_stats tr").removeClass("hover");$("#analyses_stats #tr_' . $sAnalysis . '").addClass("hover");</SCRIPT>');
+            $this->oBAR->setMessage('Running analysis ' . $sAnalysis . ' ...');
+            $this->oBAR->setProgress(0);
+
+            if (!isset($aAnalysis['sql_fetch']) || !isset($aAnalysis['fix'])) {
+                // We don't have a query to fetch the data, nor do we have a function to run over the entries.
+                // We don't have anything to do here.
+                $this->aAnalyses[$sAnalysis]['fixed'] = $aAnalysis['fixed'] = 0;
+
+            } else {
+                // We have something to analyse.
+
+                // First, for the sake of being able to see how far we are, fetch the total number of entries
+                //  we need to look at. If there is a separate query for that (if we don't know the actual
+                //  count before), run that special query. Otherwise, fallback to the normal count. If that
+                //  is also not available, fall back to the total_variants count.
+                if (!isset($aAnalysis['sql_fetch_count'])) {
+                    if (!isset($aAnalysis['count'])) {
+                        $nData = $this->aAnalyses[substr($sAnalysis, 0, 3) . '_total_variants']['count'];
+                    } else {
+                        $nData = $aAnalysis['count'];
+                    }
+                } else {
+                    // A special query was constructed to count the number of entries we need to look at.
+                    // If we would be doing a fetchAll(), then we wouldn't need this.
+                    $nData = $_DB->query($aAnalysis['sql_fetch_count']);
+                }
+                // We're optimizing for memory usage here, not speed. So we'll fetch the results line by line,
+                //  and have the fix function called for every line. This does slow things down, and requires
+                //  a separate count query, but I prefer that this script can handle any size of database.
+                $qData = $_DB->query($aAnalysis['sql_fetch']);
+                if (!isset($this->aAnalyses[$sAnalysis]['fixed'])) {
+                    $this->aAnalyses[$sAnalysis]['fixed'] = 0;
+                }
+                for ($i = 1; $zData = $qData->fetchAssoc(); $i ++) {
+                    // The fix() function analyses the data row and updates the database if needed.
+                    // It returns the number of entries it updated (0 or 1).
+                    $nUpdated = $aAnalysis['fix']($zData);
+                    $this->aAnalyses[$sAnalysis]['fixed'] += $nUpdated;
+                    $this->aAnalyses[substr($sAnalysis, 0, 3) . '_total_variants']['fixed'] += $nUpdated;
+
+                    // Update the progress.
+                    if (!($i % 10) || $i == $nData) {
+                        // Update the progress bar...
+                        $this->oBAR->setProgress($i / $nData * 100);
+                        // And show the updated counts (count/fixed, both can change).
+                        $this->updateAnalysisRow($sAnalysis);
+                    }
+                }
+            }
+            sleep(1);
+        }
+        print('
+      <SCRIPT type="text/javascript">$("#analyses_stats tr").removeClass("hover");</SCRIPT>');
+        $this->oBAR->setMessage('All done!');
+        $this->oBAR->setProgress(100);
+    }
+
+
+
+
+
+    public function printStats ()
+    {
+        // Prints the stats in a table. Could be just pre-counts or already processed results.
+
+        print('
+      <TABLE class="data" id="analyses_stats">
+        <TR>
+          <TH>Analysis</TH>
+          <TH style="text-align: right;">Count</TH>
+          <TH>Fixed</TH>
+        </TR>');
+        foreach ($this->aAnalyses as $sAnalysis => $aAnalysis) {
+            // If we don't have counts yet (analysis needed), then put a question mark.
+            if (!isset($aAnalysis['count'])) {
+                $aAnalysis['count'] = '?';
+            }
+            // If we don't have fix counts yet (fix needed), then put a hyphen.
+            if (!isset($aAnalysis['fixed'])) {
+                $aAnalysis['fixed'] = '-';
+            }
+            print('
+        <TR id="tr_' . $sAnalysis . '">
+          <TD>' . $sAnalysis . '</TD>
+          <TD style="text-align: right;">' . $aAnalysis['count'] . '</TD>
+          <TD style="text-align: right;">' . $aAnalysis['fixed'] . '</TD></TR>');
+        }
+        print('</TABLE>');
+    }
+
+
+
+
+
+    protected function updateAnalysisRow ($sAnalysis)
+    {
+        // Update the row in the table, in case the counts ("count" and "fixed" counts) changed.
+        $sTotalVariants = substr($sAnalysis, 0, 3) . '_total_variants';
+        print('
+      <SCRIPT type="text/javascript">$("#analyses_stats #tr_' . $sAnalysis . ' td:eq(2)").html("' . $this->aAnalyses[$sAnalysis]['fixed'] . '"); $("#analyses_stats #tr_' . $sTotalVariants . ' td:eq(2)").html("' . $this->aAnalyses[$sTotalVariants]['fixed'] . '");</SCRIPT>');
+    }
+}
+
+
+
+
+
+// Instantiate class, get counts.
+$_ANALYSES = new LOVD_VariantPositionAnalyses();
+
+// Print current stats in a table.
+$_ANALYSES->printStats();
+
+
+
+// If we don't have an ACTION, just print the button to start the analysis.
+if (!ACTION) {
+    print('
+      <BR>
+      <BUTTON onclick="window.location.href = \'' . CURRENT_PATH . '?run\';">Start analyses / fixes &raquo;</BUTTON>');
+    $_T->printFooter();
+    exit;
+}
+
+
+
+
+
+if (ACTION == 'run') {
+    // Actually run the analyses, update the stats table with the latest information while we run.
+    @set_time_limit(0);
+
+    $_T->printFooter(false);
+
+    $_ANALYSES->runAnalyses();
+
+    print('
+</BODY>
+</HTML>');
+    exit;
+}
+?>
