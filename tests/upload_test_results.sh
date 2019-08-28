4b07304a6514caa133cc8af04a72b06ddfb90527
Ivo Fokkema <i.f.a.c.fokkema@lumc.nl>
2019-08-28 18:28:52 +0200

    Attempt to get screenshots of failed runs in my email.

--- tests/upload_test_results.sh
+++ tests/upload_test_results.sh
@@ -4,12 +4,17 @@
 GLOB="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Loop through screenshot files (oldest to newest).
-for file in `ls -1 -t -r ${GLOB}/test_results/error_screenshots/*.png`; do
+for file in `ls -1 -t -r ${GLOB}/test_results/error_screenshots/ | grep -F .png`; do
     echo "Uploading file: ${file}";
 
     # Upload to transfer.sh, this command will output the URL on which the
     # uploaded file can be reached.
-    curl --upload-file "${file}" https://transfer.sh
+    RETURN=`curl --upload-file "${file}" https://transfer.sh`;
+    if [[ ! $RETURN == "Could not save metadata" ]];
+    then
+        # Transfer.sh service often fails.
+        mutt -s "Travis failure" -a "${file} -- I.F.A.C.Fokkema@LUMC.nl < <(echo "Travis run failed. Screenshot attached.")
+    fi
 
     rm -f "${file}"
 done
