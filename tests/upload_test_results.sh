3462d9f2bb9e07d04ee8cc4857ad893e3c828c80
Ivo Fokkema <i.f.a.c.fokkema@lumc.nl>
2019-08-29 10:35:45 +0200

    Fixed screenshot uploader properly, now.

--- tests/upload_test_results.sh
+++ tests/upload_test_results.sh
@@ -9,11 +9,13 @@ for file in `ls -1 -t -r ${GLOB}/test_results/error_screenshots/ | grep -F .png`
 
     # Upload to transfer.sh, this command will output the URL on which the
     # uploaded file can be reached.
-    RETURN=`curl --upload-file "${file}" https://transfer.sh`;
-    if [[ ! $RETURN == "Could not save metadata" ]];
+    RETURN=`curl -s -H "Max-Days: 2" --upload-file "${GLOB}/test_results/error_screenshots/${file}" https://transfer.sh`;
+    echo $RETURN;
+    if [[ $RETURN == "Could not save metadata" ]];
     then
         # Transfer.sh service often fails.
-        mutt -s "Travis failure" -a "${file}" -- I.F.A.C.Fokkema@LUMC.nl < <(echo "Travis run failed. Screenshot attached.")
+        echo "Transfer.sh failed, emailing file...";
+        mutt -s "Travis failure" -a "${GLOB}/test_results/error_screenshots/${file}" -- I.F.A.C.Fokkema@LUMC.nl < <(echo "Travis run failed. Screenshot attached.")
     fi
 
     rm -f "${file}"
