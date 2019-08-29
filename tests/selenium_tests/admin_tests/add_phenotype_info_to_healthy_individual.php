a2faec5e8c30a4dd851c652e455970f9491cf14c
Ivo Fokkema <i.f.a.c.fokkema@lumc.nl>
2019-08-29 17:05:09 +0200

    Phenotype creation test fails on all runs, completely clueless why. Random attempt to get it to work. - Cannot find what code change might have affected this. Manual tests work just fine. Absolutely clueless.

--- tests/selenium_tests/admin_tests/add_phenotype_info_to_healthy_individual.php
+++ tests/selenium_tests/admin_tests/add_phenotype_info_to_healthy_individual.php
@@ -15,7 +15,10 @@ class AddPhenotypeInfoToHealthyIndividualTest extends LOVDSeleniumWebdriverBaseT
         $element = $this->driver->findElement(WebDriverBy::xpath("//div/table/tbody/tr/td/table/tbody/tr/td[2]/b"));
         $element->click();
         $this->assertTrue((bool)preg_match('/^[\s\S]*\/src\/phenotypes[\s\S]create&target=00000001$/', $this->driver->getCurrentURL()));
-//        $this->selectWindow("null");
+
+        // This page redirects once. To prevent problems, we'll wait until we get the form.
+        $this->waitUntil(WebDriverExpectedCondition::presenceOfElementLocated(WebDriverBy::xpath("//input[@value='Create phenotype information entry']")));
+
         $this->enterValue(WebDriverBy::name("Phenotype/Age"), "35y");
         $option = $this->driver->findElement(WebDriverBy::xpath('//select[@name="owned_by"]/option[text()="LOVD3 Admin (#00001)"]'));
         $option->click();
