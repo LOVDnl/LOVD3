40d3fffa74754d2b85e64f232c1236c5deb0ed8f
Ivo Fokkema <Ivo@IFokkema.nl>
2020-04-06 12:22:58 +0200

    Added the ESYNTAX error, for non-HGVS variants sent over.

--- src/class/variant_validator.php
+++ src/class/variant_validator.php
@@ -4,7 +4,7 @@
  * LEIDEN OPEN VARIATION DATABASE (LOVD)
  *
  * Created     : 2020-03-09
- * Modified    : 2020-04-03
+ * Modified    : 2020-04-06
  * For LOVD    : 3.0-24
  *
  * Copyright   : 2004-2020 Leiden University Medical Center; http://www.LUMC.nl/
@@ -311,6 +311,9 @@ class LOVD_VV
                             } elseif (strpos($aJSON[$sVariant]['genomic_variant_error'], 'is outside the boundaries of reference sequence') !== false) {
                                 // ERANGE error.
                                 $aData['errors']['ERANGE'] = $sError;
+                            } elseif (strpos($aJSON[$sVariant]['genomic_variant_error'], 'Syntax error') !== false) {
+                                // ESYNTAX error.
+                                $aData['errors']['ESYNTAX'] = $sError;
                             } elseif (strpos($aJSON[$sVariant]['genomic_variant_error'], 'is not associated with genome build') !== false) {
                                 // EBUILD error.
                                 $aData['errors']['EREFSEQ'] = $sError;
