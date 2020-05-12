a6c5b67397265ab5268f79dc28eb2511f4cc4e28
Ivo Fokkema <Ivo@IFokkema.nl>
2020-05-12 16:24:28 +0200

    Simplify the screenshot uploader a bit, also make it upload text files.

--- tests/upload_test_results.sh
+++ tests/upload_test_results.sh
@@ -10,29 +10,32 @@ if [ `ls -1 ${GLOB}/test_results/error_screenshots/ | wc -l` -gt 0 ];
 then
   # There are error screenshots. Also check for errors in the logs.
   tail -n 50 /var/log/apache2/error.log | grep -v ':notice]';
-fi # We could put an else and nicely die with exit 0, but the code below doesn't error anymore when there are no files.
+else
+  exit 0;
+fi
 
 
 
 # Loop through screenshot files (oldest to newest).
-for file in `ls -1 -t -r ${GLOB}/test_results/error_screenshots/ | grep -F .png`; do
+for file in ${GLOB}/test_results/error_screenshots/*; do
+    echo "";
     echo "Uploading file: ${file}";
 
     # Transfer.sh is dead, again, so let's stop relying on it.
     # It is open source, so we could try to set it up on one of our own servers, though?
     # For now, upload to file.io.
     # This command will output the URL on which the uploaded file can be reached.
-    RETURN=`curl -s -F "file=@${GLOB}/test_results/error_screenshots/${file}" https://file.io?expires=1w`;
+    RETURN=`curl -s -F "file=@${file}" https://file.io?expires=1w`;
     if [[ $(echo $RETURN | cut -b 1) != '{' || $(echo $RETURN | jq .success) != 'true' ]];
     then
         # file.io service failed.
         echo "Upload failed, emailing file...";
-        mutt -s "Travis failure" -a "${GLOB}/test_results/error_screenshots/${file}" -- I.F.A.C.Fokkema@LUMC.nl < <(echo "Travis run failed. Screenshot attached.")
+        mutt -s "Travis failure" -a "${file}" -- I.F.A.C.Fokkema@LUMC.nl < <(echo "Travis run failed. Screenshot attached.")
     else
         echo $RETURN | jq -r .link;
         echo -n 'Expires in ';
-        echo -n $RETURN | jq -r .expiry;
+        echo $RETURN | jq -r .expiry;
     fi
 
-    rm -f "${GLOB}/test_results/error_screenshots/${file}"
+    rm -f "${file}"
 done
