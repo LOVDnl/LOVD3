fed9301caa7651b5cc4a5b5b12687dfdc6b04422
Ivo Fokkema <Ivo@IFokkema.nl>
2020-05-14 13:56:44 +0200

    Updated .travis.yml and Travis setup scripts. - After this, we're ready to merge in to master. - More work is needed to perfect this, but these changes will allow us to run our tests again.

--- tests/travis/setup/setup_mysql.sh
+++ tests/travis/setup/setup_mysql.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-# Finish upgrade of mysql 5.6 to mysql 5.7 (installed using apt directive in travis.yml).
-#sudo mysql_upgrade
-#sudo service mysql restart
-
-## This user/login information here should be in accordance with setup_config.sh
+# This user/login information here should be in accordance with setup_config.sh
 echo "Setup mysql"
 mysql -e "create database IF NOT EXISTS lovd3_development;" -u root
 mysql -e "CREATE USER 'lovd'@'localhost' IDENTIFIED BY 'lovd_pw';" -u root
