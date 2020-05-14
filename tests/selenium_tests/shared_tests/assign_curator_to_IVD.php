6f7fceaf430f39f72777c7c58aaab7c027639c39
Ivo Fokkema <Ivo@IFokkema.nl>
2020-05-14 11:17:22 +0200

    Updated test for assigning the Curator to the IVD gene. - There were two tests, doing exactly the same - removed redundancy. - Updated test to follow coding standards, by adding a header, a setUp() method and easier test methods.

---
+++ tests/selenium_tests/shared_tests/assign_curator_to_IVD.php
@@ -0,0 +1,76 @@
+<?php
+/*******************************************************************************
+ *
+ * LEIDEN OPEN VARIATION DATABASE (LOVD)
+ *
+ * Created     : 2015-02-17
+ * Modified    : 2020-05-14
+ * For LOVD    : 3.0-24
+ *
+ * Copyright   : 2004-2020 Leiden University Medical Center; http://www.LUMC.nl/
+ * Programmers : M. Kroon <m.kroon@lumc.nl>
+ *               Ivo F.A.C. Fokkema <I.F.A.C.Fokkema@LUMC.nl>
+ *
+ *
+ * This file is part of LOVD.
+ *
+ * LOVD is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LOVD is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LOVD.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *************/
+
+require_once 'LOVDSeleniumBaseTestCase.php';
+
+use \Facebook\WebDriver\WebDriverBy;
+
+class AssignCuratorToIVDTest extends LOVDSeleniumWebdriverBaseTestCase
+{
+    protected function setUp ()
+    {
+        // Test if we have what we need for this test. If not, skip this test.
+        parent::setUp();
+        $this->driver->get(ROOT_URL . '/src/genes/IVD');
+        $sBody = $this->driver->findElement(WebDriverBy::tagName('body'))->getText();
+        if (preg_match('/LOVD was not installed yet/', $sBody)) {
+            $this->markTestSkipped('LOVD was not installed yet.');
+        }
+        if (preg_match('/No such ID!/', $sBody)) {
+            $this->markTestSkipped('Gene does not exist yet.');
+        }
+
+        // Curator is user ID 3.
+        $this->driver->get(ROOT_URL . '/src/users/00003');
+        $sBody = $this->driver->findElement(WebDriverBy::tagName('body'))->getText();
+        if (preg_match('/To access this area/', $sBody)) {
+            $this->markTestSkipped('User was not authorized.');
+        }
+        if (preg_match('/No such ID!/', $sBody)) {
+            $this->markTestSkipped('User does not exist yet.');
+        }
+    }
+
+
+
+
+
+    public function test ()
+    {
+        $this->driver->get(ROOT_URL . "/src/genes/IVD?authorize");
+        $element = $this->driver->findElement(WebDriverBy::linkText("Test Curator"));
+        $element->click();
+        $this->enterValue(WebDriverBy::name('password'), 'test1234');
+        $this->submitForm('Save curator list');
+        $this->assertEquals('Successfully updated the curator list!',
+            $this->driver->findElement(WebDriverBy::cssSelector('table[class=info]'))->getText());
+    }
+}
