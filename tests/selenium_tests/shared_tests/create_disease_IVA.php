fe3928642f0e745f267905d8859ea5b8aff19fd3
Ivo Fokkema <Ivo@IFokkema.nl>
2020-05-15 12:32:26 +0200

    Updated test for creating the IVA disease. - Updated test to follow coding standards, by adding a header, a setUp() method and easier test methods.

--- tests/selenium_tests/shared_tests/create_disease_IVA.php
+++ tests/selenium_tests/shared_tests/create_disease_IVA.php
@@ -1,22 +1,71 @@
 <?php
+/*******************************************************************************
+ *
+ * LEIDEN OPEN VARIATION DATABASE (LOVD)
+ *
+ * Created     : 2015-02-17
+ * Modified    : 2020-05-15
+ * For LOVD    : 3.0-24
+ *
+ * Copyright   : 2004-2020 Leiden University Medical Center; http://www.LUMC.nl/
+ * Programmers : M. Kroon <m.kroon@lumc.nl>
+ *               Ivo F.A.C. Fokkema <I.F.A.C.Fokkema@LUMC.nl>
+ *
+ *
+ * This file is part of LOVD.
+ *
+ * LOVD is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LOVD is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LOVD.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *************/
+
 require_once 'LOVDSeleniumBaseTestCase.php';
 
 use \Facebook\WebDriver\WebDriverBy;
-use \Facebook\WebDriver\WebDriverExpectedCondition;
 
 class CreateDiseaseIVATest extends LOVDSeleniumWebdriverBaseTestCase
 {
-    public function testCreateDiseaseIVA()
+    protected function setUp ()
+    {
+        // Test if we have what we need for this test. If not, skip this test.
+        parent::setUp();
+        // IVA is disease ID 1.
+        $this->driver->get(ROOT_URL . '/src/diseases/00001');
+        $sBody = $this->driver->findElement(WebDriverBy::tagName('body'))->getText();
+        if (preg_match('/LOVD was not installed yet/', $sBody)) {
+            $this->markTestSkipped('LOVD was not installed yet.');
+        }
+        if (!preg_match('/No such ID!/', $sBody)) {
+            $this->markTestSkipped('Disease was already created.');
+        }
+        if (!$this->isElementPresent(WebDriverBy::id('tab_setup'))) {
+            $this->markTestSkipped('User was not authorized.');
+        }
+    }
+
+
+
+
+
+    public function test ()
     {
         $this->driver->get(ROOT_URL . "/src/diseases?create");
-        $this->enterValue(WebDriverBy::name("symbol"), "IVA");
-        $this->enterValue(WebDriverBy::name("name"), "isovaleric acidemia");
-        $this->enterValue(WebDriverBy::name("id_omim"), "243500");
-        $option = $this->driver->findElement(WebDriverBy::xpath('//select[@name="genes[]"]/option[text()="IVD (isovaleryl-CoA dehydrogenase)"]'));
-        $option->click();
-        $element = $this->driver->findElement(WebDriverBy::xpath("//input[@value='Create disease information entry']"));
-        $element->click();
-        
-        $this->assertEquals("Successfully created the disease information entry!", $this->driver->findElement(WebDriverBy::cssSelector("table[class=info]"))->getText());
+        $this->enterValue(WebDriverBy::name('symbol'), 'IVA');
+        $this->enterValue(WebDriverBy::name('name'), 'isovaleric acidemia');
+        $this->enterValue(WebDriverBy::name('id_omim'), '243500');
+        $this->selectValue('genes[]', 'IVD (isovaleryl-CoA dehydrogenase)');
+        $this->submitForm('Create disease information entry');
+        $this->assertEquals('Successfully created the disease information entry!',
+            $this->driver->findElement(WebDriverBy::cssSelector('table[class=info]'))->getText());
     }
 }
