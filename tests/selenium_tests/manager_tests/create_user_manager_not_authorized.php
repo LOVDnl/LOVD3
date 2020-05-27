0152f5ffed3ea9f784b9f282c1770d4973899420
Ivo Fokkema <Ivo@IFokkema.nl>
2020-05-27 09:01:07 +0200

    Rebuilt test for not being authorized creating a Manager. - Renamed the test because it was unclear.

---
+++ tests/selenium_tests/manager_tests/create_user_manager_not_authorized.php
@@ -0,0 +1,61 @@
+<?php
+/*******************************************************************************
+ *
+ * LEIDEN OPEN VARIATION DATABASE (LOVD)
+ *
+ * Created     : 2015-02-17
+ * Modified    : 2020-05-27
+ * For LOVD    : 3.0-24
+ *
+ * Copyright   : 2004-2020 Leiden University Medical Center; http://www.LUMC.nl/
+ * Programmer  : Ivo F.A.C. Fokkema <I.F.A.C.Fokkema@LUMC.nl>
+ *
+ *
+ * This file is part of LOVD.
+ *
+ * LOVD is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LOVD is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LOVD.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *************/
+
+require_once 'LOVDSeleniumBaseTestCase.php';
+
+use \Facebook\WebDriver\WebDriverBy;
+
+class CreateUserManagerNotAuthorizedTest extends LOVDSeleniumWebdriverBaseTestCase
+{
+    protected function setUp ()
+    {
+        // Test if we have what we need for this test. If not, skip this test.
+        parent::setUp();
+        $this->driver->get(ROOT_URL . '/src/users?create');
+        $sBody = $this->driver->findElement(WebDriverBy::tagName('body'))->getText();
+        if (preg_match('/LOVD was not installed yet/', $sBody)) {
+            $this->markTestSkipped('LOVD was not installed yet.');
+        }
+        if (preg_match('/To access this area/', $sBody)) {
+            $this->markTestSkipped('User was not authorized.');
+        }
+    }
+
+
+
+
+
+    public function test ()
+    {
+        $this->driver->get(ROOT_URL . '/src/users?create&no_orcid');
+        $this->assertFalse($this->isElementPresent(WebDriverBy::xpath(
+            '//select[@name="level"]/option[text()="Manager"]')));
+    }
+}
