6325b0174bac518583331c4d55674e6e907e1b59
Ivo Fokkema <Ivo@IFokkema.nl>
2020-06-16 14:04:22 +0200

    Added test not being authorized for creating a gene. - This test also got added to the other relevant test suites. - Added files that I forgot to commit previously; the updated .travis.yml and the test for logging in as owner.

---
+++ tests/selenium_tests/shared_tests/login_as_owner.php
@@ -0,0 +1,62 @@
+<?php
+/*******************************************************************************
+ *
+ * LEIDEN OPEN VARIATION DATABASE (LOVD)
+ *
+ * Created     : 2020-06-16
+ * Modified    : 2020-06-16
+ * For LOVD    : 3.0-24
+ *
+ * Copyright   : 2004-2020 Leiden University Medical Center; http://www.LUMC.nl/
+ * Programmer  : Ivo F.A.C. Fokkema <I.F.A.C.Fokkema@LUMC.nl>
+ *
+ *
+ * This file is part of LOVD.
+ *
+ * LOVD is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LOVD is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LOVD.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *************/
+
+require_once 'LOVDSeleniumBaseTestCase.php';
+
+use \Facebook\WebDriver\WebDriverBy;
+
+class LoginAsOwnerTest extends LOVDSeleniumWebdriverBaseTestCase
+{
+    protected function setUp ()
+    {
+        // Test if we have what we need for this test. If not, skip this test.
+        parent::setUp();
+        // Owner is user ID 5.
+        $this->driver->get(ROOT_URL . '/src/users/00005');
+        $sBody = $this->driver->findElement(WebDriverBy::tagName('body'))->getText();
+        if (preg_match('/LOVD was not installed yet/', $sBody)) {
+            $this->markTestSkipped('LOVD was not installed yet.');
+        }
+        if (preg_match('/No such ID!/', $sBody)) {
+            $this->markTestSkipped('User does not exist yet.');
+        }
+    }
+
+
+
+
+
+    public function test ()
+    {
+        $this->logout();
+        $this->login('owner', 'test1234');
+    }
+}
+?>
