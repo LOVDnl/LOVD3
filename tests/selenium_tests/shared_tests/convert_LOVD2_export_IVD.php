4f87183d2ad8c4d28bee8b53e6296fc47dcbbd58
Ivo Fokkema <Ivo@IFokkema.nl>
2020-06-18 11:55:47 +0200

    Rebuilt and improved LOVD2 data file conversion test. - Click the transcript instead of knowing the ID (even though the result verification currently requires the ID to be static). - Now also checking for the warnings that the script is throwing.

--- tests/selenium_tests/shared_tests/convert_LOVD2_export_IVD.php
+++ tests/selenium_tests/shared_tests/convert_LOVD2_export_IVD.php
@@ -4,11 +4,12 @@
  * LEIDEN OPEN VARIATION DATABASE (LOVD)
  *
  * Created     : 2017-06-27
- * Modified    : 2017-07-06
- * For LOVD    : 3.0-20
+ * Modified    : 2020-06-18
+ * For LOVD    : 3.0-24
  *
- * Copyright   : 2017 Leiden University Medical Center; http://www.LUMC.nl/
+ * Copyright   : 2004-2020 Leiden University Medical Center; http://www.LUMC.nl/
  * Programmers : M. Kroon <m.kroon@lumc.nl>
+ *               Ivo F.A.C. Fokkema <I.F.A.C.Fokkema@LUMC.nl>
  *
  *
  * This file is part of LOVD.
@@ -31,40 +32,58 @@
 require_once 'LOVDSeleniumBaseTestCase.php';
 
 use \Facebook\WebDriver\WebDriverBy;
-use \Facebook\WebDriver\WebDriverExpectedCondition;
 
-class ConvertLOVD2ExportTest extends LOVDSeleniumWebdriverBaseTestCase
+class ConvertLOVD2ExportIVDTest extends LOVDSeleniumWebdriverBaseTestCase
 {
-    public function testConvertLOVD2Export()
+    protected function setUp ()
+    {
+        parent::setUp();
+        $this->driver->get(ROOT_URL . '/src/genes/IVD');
+        $sBody = $this->driver->findElement(WebDriverBy::tagName('body'))->getText();
+        if (preg_match('/LOVD was not installed yet/', $sBody)) {
+            $this->markTestSkipped('LOVD was not installed yet.');
+        }
+        if (preg_match('/No such ID!/', $sBody)) {
+            $this->markTestSkipped('Gene does not exist yet.');
+        }
+    }
+
+
+
+
+
+    public function test ()
     {
         // Test functionality of conversion script for LOVD2 export to LOVD3 import formats.
-
         $this->driver->get(ROOT_URL . '/src/scripts/convert_lovd2.php');
-
-        $buttonLocator = WebDriverBy::xpath('//input[@value="Generate LOVD3 import file"]');
-        $this->waitUntil(WebDriverExpectedCondition::presenceOfElementLocated($buttonLocator));
-
-        $this->enterValue(WebDriverBy::name('transcriptid'), '1');
-        $this->enterValue(WebDriverBy::name('LOVD2_export'), ROOT_PATH .
+        $this->driver->findElement(WebDriverBy::xpath(
+            '//table[@class="data"]//tr[td[text()="IVD"]]/td[contains(text(), "NM_002225.")]'))->click();
+        // If IVD's NM_002225 is not going to be transcript 1 anymore, the verification of the data file will fail anyway.
+        // If we want to cater for that possibility, we'll need to be more dynamic about verifying the contents of the output file.
+        $this->assertEquals('00000001', $this->driver->findElement(WebDriverBy::name('transcriptid'))->getAttribute('value'));
+        $this->enterValue('LOVD2_export', ROOT_PATH .
             '../tests/test_data_files/LOVD2_conversion/test_lovd2export.txt');
-
-        $sSubmitterTransTable = file_get_contents(ROOT_PATH .
-            '../tests/test_data_files/LOVD2_conversion/test_submitter_translation.txt');
-        $this->enterValue(WebDriverBy::name('submitterid_translation'), $sSubmitterTransTable);
+        $this->enterValue('submitterid_translation', file_get_contents(ROOT_PATH .
+            '../tests/test_data_files/LOVD2_conversion/test_submitter_translation.txt'));
 
         // Start conversion.
-        $this->driver->findElement($buttonLocator)->click();
+        $this->driver->findElement(WebDriverBy::xpath(
+            '//input[@value="Generate LOVD3 import file"]'))->click();
+        $this->waitForElement(WebDriverBy::id('copybutton'));
 
-        $copyButtonLocator = WebDriverBy::id('copybutton');
-        $this->waitUntil(WebDriverExpectedCondition::presenceOfElementLocated($copyButtonLocator));
+        // Compare results.
+        $this->assertEquals(
+            rtrim(file_get_contents(ROOT_PATH . '../tests/test_data_files/LOVD2_conversion/result_conversion_log.txt')),
+            $this->driver->findElement(WebDriverBy::id('header_log'))->getText()
+        );
 
-        // Compare generated output with expected output from file. (Skip first line in output)
-        $sExpectedOutput = file_get_contents(ROOT_PATH .
-            '../tests/test_data_files/LOVD2_conversion/test_output.txt');
-        $outputArea = $this->driver->findElement(WebDriverBy::id('conversion_output'));
-        $aOutput = explode("\n", $outputArea->getAttribute('value'));
-        array_shift($aOutput); // Remove first line (holds LOVD version)
-        $sOutputTail = join("\n", $aOutput);
-        $this->assertEquals($sExpectedOutput, $sOutputTail);
+        // Skip first line in output, which holds the LOVD version.
+        $aOutput = explode("\n", $this->driver->findElement(
+            WebDriverBy::id('conversion_output'))->getAttribute('value'));
+        array_shift($aOutput);
+        $sOutputTail = implode("\n", $aOutput);
+        $this->assertEquals(file_get_contents(ROOT_PATH .
+            '../tests/test_data_files/LOVD2_conversion/result_output.txt'), $sOutputTail);
     }
 }
+?>
