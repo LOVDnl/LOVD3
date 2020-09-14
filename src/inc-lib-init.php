867a1df1c076666e929294022720e9b5712f2131
Ivo Fokkema <Ivo@IFokkema.nl>
2020-09-14 17:20:19 +0200

    Updated lovd_getCurrentID() to support references.

--- src/inc-lib-init.php
+++ src/inc-lib-init.php
@@ -679,9 +679,13 @@ function lovd_getCurrentID ()
     if (PATH_COUNT == 3 && $_PE[0] == 'phenotypes' && $_PE[1] == 'disease') {
         // Disease-specific list of phenotypes; /phenotypes/disease/00001.
         return $_PE[2];
-    } elseif (PATH_COUNT >= 2 && $_PE[0] == 'columns') {
-        // For columns, the ID al all of $_PE.
-        return implode('/', array_slice($_PE, 1));
+    } elseif (PATH_COUNT >= 2 && in_array($_PE[0], array('columns', 'references'))) {
+        // For columns and references, the ID al all of $_PE.
+        $sID = implode('/', array_slice($_PE, 1));
+        if ($_PE[0] == 'references') {
+            $sID = preg_replace('/\/image$/', '', $sID);
+        }
+        return $sID;
     } elseif (PATH_COUNT >= 2) {
         return $_PE[1]; // 0-padding has already been done in inc-init.php.
     }
