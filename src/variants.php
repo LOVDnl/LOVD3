56bd09190621a08dfff2c3a2fb9350512b7cbdef
Ivo Fokkema <Ivo@IFokkema.nl>
2020-11-02 15:41:10 +0100

    Disabled the variants/in_gene view, unless we're coming from the world-wide search engine.

--- src/variants.php
+++ src/variants.php
@@ -4,7 +4,7 @@
  * LEIDEN OPEN VARIATION DATABASE (LOVD)
  *
  * Created     : 2010-12-21
- * Modified    : 2020-10-28
+ * Modified    : 2020-11-02
  * For LOVD    : 3.0-26
  *
  * Copyright   : 2004-2020 Leiden University Medical Center; http://www.LUMC.nl/
@@ -187,7 +187,8 @@ if (!ACTION && (empty($_PE[1])
 
 
 
-if (PATH_COUNT == 2 && $_PE[1] == 'in_gene' && !ACTION) {
+if (PATH_COUNT == 2 && $_PE[1] == 'in_gene' && !ACTION
+    && (!LOVD_light || (!empty($_GET['search_geneid']) && !empty('search_VariantOnTranscript/DNA')))) {
     // URL: /variants/in_gene
     // View all entries effecting a transcript.
 
