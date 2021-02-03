1fcf82cf681220b1ec2750633fa49a7e6b034218
Ivo Fokkema <Ivo@IFokkema.nl>
2021-02-03 11:09:09 +0100

    Added first version of the donation dialog.

---
+++ src/ajax/donate.php
@@ -0,0 +1,59 @@
+<?php
+/*******************************************************************************
+ *
+ * LEIDEN OPEN VARIATION DATABASE (LOVD)
+ *
+ * Created     : 2021-02-01
+ * Modified    : 2021-02-03
+ * For LOVD    : 3.0-26
+ *
+ * Copyright   : 2004-2021 Leiden University Medical Center; http://www.LUMC.nl/
+ * Programmer  : Ivo F.A.C. Fokkema <I.F.A.C.Fokkema@LUMC.nl>
+ *
+ *
+ * This file is part of LOVD.
+ *
+ * LOVD is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LOVD is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LOVD.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *************/
+
+header('Content-type: text/javascript; charset=UTF-8');
+?>
+// Make sure we have and show the dialog.
+if (!$("#donate_dialog").length) {
+    $("body").append("<DIV id='donate_dialog' title='Donate to the LOVD project'>Dear user,<BR><BR>The LOVD software that runs this database is provided free of charge to the scientific community. However, creating and maintaining it as well as keeping the servers running that host the software and the central GV shared LOVD instance, is not free.<BR>Please consider making a donation to support us, through a donation to Global Variome / The Human Variome Project.<IMG src='gfx/donate_qr_100x100.png' alt='' width='100' height='100' style='float: right;'><BR><BR>Thank you in advance for your support!<BR>The LOVD team, Leiden, Netherlands<BR><BR><A href='https://www.paypal.com/donate/?hosted_button_id=DHJVLF3Z2TA2U' target='_blank'><IMG src='https://www.paypalobjects.com/en_GB/i/btn/btn_donate_LG.gif' border='0' title='PayPal - The safer, easier way to pay online!' alt='Donate with PayPal button'></A></DIV>");
+}
+if (!$("#donate_dialog").hasClass("ui-dialog-content") || !$("#donate_dialog").dialog("isOpen")) {
+    $("#donate_dialog").dialog(
+    {
+        draggable:true,resizable:false,minWidth:500,show:"fade",closeOnEscape:true,hide:"fade",modal:false,position:{my:"top",at:"bottom",of:$("#stickyheader").siblings("table")[0]},
+        // Stick Dialog to the screen, not the DOM.
+        // Thanks to msander @ https://stackoverflow.com/questions/2657076/jquery-ui-dialog-fixed-positioning
+        create: function (event, ui) {
+            $(event.target).parent().css('position', 'fixed');
+        },
+        resizeStop: function (event, ui) {
+            var position = [(Math.floor(ui.position.left) - $(window).scrollLeft()),
+                            (Math.floor(ui.position.top) - $(window).scrollTop())];
+            $(event.target).parent().css('position', 'fixed');
+            $("#donate_dialog").dialog('option','position',position);
+        },
+        open: function (event, ui) {
+            // Remove the focus off of the donate button.
+            // Blur() doesn't remove the focus as many suggest. This does work.
+            // Thanks to scott.gonzalez @ https://forum.jquery.com/topic/dialog-close-button-is-focused-on-open
+            $(this).parents('.ui-dialog').attr('tabindex', -1)[0].focus();
+        }
+    });
+}
