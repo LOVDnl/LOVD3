324af602b199cf613bb28c9ff79df9d72a2d9f43
loeswerkman <loesjewerkman@live.nl>
2021-09-27 07:54:01 +0200

    Activate ?add of GBs; Insert added GB into GB table
    also: polish up typo in inc-lib-form

--- src/genome_builds.php
+++ src/genome_builds.php
@@ -47,12 +47,14 @@ if (PATH_COUNT == 1 && !ACTION) {
     // View all genome builds.
 
     define('PAGE_TITLE', 'Genome builds');
-    $_T->printHeader();
-    $_T->printTitle();
 
     lovd_requireAUTH(LEVEL_MANAGER);
 
     require ROOT_PATH . 'class/object_genome_builds.php';
+
+    $_T->printHeader();
+    $_T->printTitle();
+
     $_DATA = new LOVD_GenomeBuild();
     $_DATA->viewList('GenomeBuilds', array());
 
@@ -61,8 +63,104 @@ if (PATH_COUNT == 1 && !ACTION) {
 }
 
 
+
+
+
+if (PATH_COUNT == 1 && ACTION == 'add') {
+    // URL: /genome_builds?add
+    // Enable a new genome builds.
+
+    define('PAGE_TITLE', lovd_getCurrentPageTitle());
+    define('LOG_EVENT', 'GenomeBuildAdd');
+
+    lovd_requireAUTH(LEVEL_MANAGER); // TODO: Place to top of file
+
+    require ROOT_PATH . 'inc-lib-form.php';
+    require ROOT_PATH . 'class/object_genome_builds.php';
+    $_DATA = new LOVD_GenomeBuild();
+
+    // Apply the choices as filled into the form.
+    if (POST) {
+        lovd_errorClean();
+
+        // TODO: Add checks: Formulier moet zijn ingevuld, ID moet bestaan
+
+        if (!lovd_error()) {
+            // Fields to be used.
+            $aFields = array('id', 'name', 'column_suffix', 'created_by', 'created_date');
+
+            // Prepare values.
+            $_POST['name'] = $_POST['id'] . ' / ' . $_SETT['human_builds'][$_POST['id']]['ncbi_name'];
+            $_POST['column_suffix'] = $_POST['id'];
+            $_POST['created_by'] = $_AUTH['id'];
+            $_POST['created_date'] = date('Y-m-d H:i:s');
+
+            // Add new genome build as new row into GenomeBuilds table.
+            $_DATA->insertEntry($_POST, $aFields);
+
+            // Write to log...
+            lovd_writeLog('Event', LOG_EVENT, 'Added new Genome Build ' . $_POST['id']);
+
+
+            // Thank the user...
+            header('Refresh: 5; url=' . lovd_getInstallURL(). CURRENT_PATH . '/' . $_POST['id']);
+
+            $_T->printHeader();
+            $_T->printTitle();
+            lovd_showInfoTable('Successfully added the new genome build!', 'success');
+
+            $_T->printFooter();
+            exit;
+        }
+    }
+
+    $_T->printHeader();
+    $_T->printTitle();
+
+    // Array which will make up the form table.
+    $aActiveBuilds = $_DB->query('SELECT id FROM ' . TABLE_GENOME_BUILDS)->fetchAllColumn();
+    $sPrefix = substr($aActiveBuilds[0], 0, 2);
+
+    $aAddableGenomeBuilds = array();
+    foreach ($_SETT['human_builds'] as $sBuild => $aBuild) {
+        if (substr($sBuild, 0, 2) == $sPrefix &&  !in_array($sBuild, $aActiveBuilds)) {
+            $aAddableGenomeBuilds[$sBuild] = $sBuild . ' / ' . $aBuild['ncbi_name'];
+        }
+    };
+
+    // TODO: Only show form when there are still addable yet inactive GBs left
+    
+    if (GET) {
+        print('Please select the genome build you want to add to your database.<BR><BR>');
+    }
+
+    // Tooltip JS code.
+    lovd_includeJS('inc-js-tooltip.php');
+
+    // Table.
+    print('      <FORM action="' . CURRENT_PATH . '?' . ACTION . '" method="post">' . "\n");
+
+    $aForm =
+        array(
+            array('POST', '', '', '', '0%', '14', '100%'),
+            array('', '', 'select', 'id', count($aAddableGenomeBuilds), $aAddableGenomeBuilds, false, false, false),
+            'skip',
+            array('', '', 'submit', 'Add selected genome build'),
+        );
+
+    lovd_viewForm($aForm);
+
+    print('</FORM>' . "\n\n");
+    $_T->printFooter();
+    exit;
+}
+
+
+
+
+
 if (PATH_COUNT == 2 && !ACTION) {
-    // URL: /genomebuilds/hg19
+    // URL: /genome_builds/hg19
     // View specific genome build.
 
     $sID = lovd_getCurrentID();
