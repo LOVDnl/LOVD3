998fd1b8c47206bbdb72b744bd72f1bc70ca6af1
loeswerkman <loesjewerkman@live.nl>
2021-10-07 10:49:42 +0200

    Allow element data to be added into HTML via viewForm

--- src/class/object_custom.php
+++ src/class/object_custom.php
@@ -295,6 +295,11 @@ class LOVD_Custom extends LOVD_Object
                 $aFormData[$sPrefix . $sCol] = $aEntry;
             }
 
+            // The element data will be passed on, so that viewForm can make sure it will be added to the HTML.
+            if (!empty($aCol['element_data'])) {
+                $aFormData[$sPrefix . $sCol][] = $aCol['element_data'];
+            }
+
             // Any custom links we want to mention?
             if (!empty($aCol['custom_links'])) {
                 $sLinks = '';
