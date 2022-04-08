0c9d438e348f36d639efec624df3fe3eb027d591
Loes Werkman <loesjewerkman@live.nl>
2022-04-08 14:29:55 +0200

    Remove indices when disabling a GB

--- src/genome_builds.php
+++ src/genome_builds.php
@@ -372,6 +372,20 @@ if (PATH_COUNT == 2 && ACTION == 'remove') {
                 }
 
                 if ($bToRemove) {
+                    if ($sTable == TABLE_VARIANTS) {
+                        // If we are working with the variants table, we must
+                        //  additionally remove the indices specific to the GB.
+                        $sKeysAndIndexInfo = $_DB->query('SHOW CREATE TABLE ' . TABLE_VARIANTS)->fetchAllAssoc();
+                        if (preg_match(
+                            '/KEY `(.*)` \(`chromosome`,`position_g_start' . $sSuffixWithUnderscore . '`,`position_g_end' . $sSuffixWithUnderscore . '`\)/',
+                            $sKeysAndIndexInfo[0]['Create Table'], $aRegs)) {
+                            // We retrieve the name of the index in the list of
+                            //  indices as found through the SHOW CREATE TABLE
+                            //  query. If we found a matching index, we will
+                            //  remove this index along with the position fields.
+                            $sSQL .= ' DROP INDEX `' . $aRegs[1] . '`';
+                        }
+                    }
                     $_DB->query(rtrim($sSQL, ','));
                 }
             }
