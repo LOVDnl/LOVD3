19968594dba3c08672843bdaf9af3ec556528fb4
Ivo Fokkema <i.f.a.c.fokkema@lumc.nl>
2022-08-31 09:42:09 +0200

    Improve styling a bit more, allow proper wrapping of long messages.

--- src/scripts/check_HGVS.php
+++ src/scripts/check_HGVS.php
@@ -180,26 +180,26 @@ NC_000015.9:g.40699840C>T" rows="3"></textarea>
                         var sBody = '<ul class="list-group list-group-flush">';
                         if (aVariant.is_hgvs == null) {
                             sBody +=
-                                '<li class="list-group-item list-group-item-' + sStyle + '"><i class="bi bi-' + sIcon + '"></i>' +
+                                '<li class="list-group-item list-group-item-' + sStyle + ' d-flex"><i class="bi bi-' + sIcon + ' me-2"></i><div>' +
                                 ' This variant description contains unsupported syntax.' +
                                 ' Although we aim to support all of the HGVS nomenclature rules,' +
-                                ' some complex variants are not fully implemented yet in our syntax checker.</li>\n';
+                                ' some complex variants are not fully implemented yet in our syntax checker.</div></li>\n';
                         } else if (!aVariant.is_hgvs) {
                             sBody +=
-                                '<li class="list-group-item list-group-item-' + sStyle + '"><i class="bi bi-' + sIcon + '"></i>' +
-                                ' This variant description is invalid.</li>\n';
+                                '<li class="list-group-item list-group-item-' + sStyle + ' d-flex"><i class="bi bi-' + sIcon + ' me-2"></i><div>' +
+                                ' This variant description is invalid.</div></li>\n';
                         } else {
                             sBody +=
-                                '<li class="list-group-item list-group-item-' + sStyle + '"><i class="bi bi-' + sIcon + '"></i>' +
-                                ' This variant description\'s syntax is valid.</li>\n';
+                                '<li class="list-group-item list-group-item-' + sStyle + ' d-flex"><i class="bi bi-' + sIcon + ' me-2"></i><div>' +
+                                ' This variant description\'s syntax is valid.</div></li>\n';
                             if (!bCallVV) {
                                 sBody +=
-                                    '<li class="list-group-item list-group-item-warning"><i class="bi bi-exclamation-circle-fill"></i>' +
+                                    '<li class="list-group-item list-group-item-warning d-flex"><i class="bi bi-exclamation-circle-fill me-2"></i><div>' +
                                     ' This variant has not been validated on the sequence level.' +
                                     ('WNOTSUPPORTED' in aVariant.variant_info.warnings?
-                                     ' This variant description is not currently supported for sequence-level validation.' :
-                                     ' For sequence-level validation, please select the VariantValidator option.')
-                                    '</li>\n';
+                                        ' This variant description is not currently supported for sequence-level validation.' :
+                                        ' For sequence-level validation, please select the VariantValidator option.') +
+                                    '</div></li>\n';
                             }
                         }
                         sBody += '</ul>';
