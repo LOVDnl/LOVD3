9bbf69806222f43d0bff96fafcc80d92f7320576
Ivo F.A.C. Fokkema <i.f.a.c.fokkema@lumc.nl>
2024-05-27 21:44:27 +0200

    Make sure the owner login test has an assertion.
    That way, it won't be marked as a Risky test.

--- tests/selenium_tests/shared_tests/login_as_owner.php
+++ tests/selenium_tests/shared_tests/login_as_owner.php
@@ -4,10 +4,10 @@
  * LEIDEN OPEN VARIATION DATABASE (LOVD)
  *
  * Created     : 2020-06-16
- * Modified    : 2020-06-16
- * For LOVD    : 3.0-24
+ * Modified    : 2024-05-27
+ * For LOVD    : 3.0-30
  *
- * Copyright   : 2004-2020 Leiden University Medical Center; http://www.LUMC.nl/
+ * Copyright   : 2004-2024 Leiden University Medical Center; http://www.LUMC.nl/
  * Programmer  : Ivo F.A.C. Fokkema <I.F.A.C.Fokkema@LUMC.nl>
  *
  *
@@ -57,6 +57,7 @@ class LoginAsOwnerTest extends LOVDSeleniumWebdriverBaseTestCase
     {
         $this->logout();
         $this->login('owner', 'test1234');
+        $this->assertStringContainsString('/src/genes', $this->driver->getCurrentURL());
     }
 }
 ?>
