6517cba9c72c2a57f2bb41115b6129585698dd81
Ivo Fokkema <i.f.a.c.fokkema@lumc.nl>
2024-11-26 10:20:00 +0100

    Fix bug; the data array is for sortable positions.

--- src/class/HGVS.php
+++ src/class/HGVS.php
@@ -1326,8 +1326,8 @@ class HGVS_DNAPositions extends HGVS
 
         // Store the positions in the data array.
         $aPositions = ($this->range? [$this->DNAPositionStart, $this->DNAPositionEnd] : [$this->DNAPosition, $this->DNAPosition]);
-        $this->data['position_start'] = $aPositions[0]->position;
-        $this->data['position_end'] = $aPositions[1]->position;
+        $this->data['position_start'] = $aPositions[0]->position_sortable;
+        $this->data['position_end'] = $aPositions[1]->position_sortable;
         if ($VariantPrefix && $VariantPrefix->molecule_type == 'transcript') {
             $this->data['position_start_intron'] = $aPositions[0]->offset;
             $this->data['position_end_intron'] = $aPositions[1]->offset;
