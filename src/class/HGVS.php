0239e2c91bd4f8998deaca2482fb613f38bbe6a3
Ivo Fokkema <i.f.a.c.fokkema@lumc.nl>
2024-12-02 10:10:00 +0100

    Don't overwrite the data array when positions are used for insertions.

--- src/class/HGVS.php
+++ src/class/HGVS.php
@@ -1546,16 +1546,18 @@ class HGVS_DNAPositions extends HGVS
             }
         }
 
-        // Store the positions in the data array.
-        $aPositions = ($this->range? [$this->DNAPositionStart, $this->DNAPositionEnd] : [$this->DNAPosition, $this->DNAPosition]);
-        // Unknown positions have no sortable position and store their extremes.
-        $this->data['position_start'] = ($aPositions[0]->position_sortable ?? $aPositions[0]->position_limits[0]);
-        $this->data['position_end'] = ($aPositions[1]->position_sortable ?? $aPositions[1]->position_limits[1]);
-        if ($VariantPrefix && $VariantPrefix->molecule_type == 'transcript') {
-            $this->data['position_start_intron'] = $aPositions[0]->offset;
-            $this->data['position_end_intron'] = $aPositions[1]->offset;
+        // Store the positions in the data array, but only if we're Positions directly under HGVS_DNAVariantBody.
+        if (get_class($this->parent) == 'HGVS_DNAVariantBody') {
+            $aPositions = ($this->range? [$this->DNAPositionStart, $this->DNAPositionEnd] : [$this->DNAPosition, $this->DNAPosition]);
+            // Unknown positions have no sortable position and store their extremes.
+            $this->data['position_start'] = ($aPositions[0]->position_sortable ?? $aPositions[0]->position_limits[0]);
+            $this->data['position_end'] = ($aPositions[1]->position_sortable ?? $aPositions[1]->position_limits[1]);
+            if ($VariantPrefix && $VariantPrefix->molecule_type == 'transcript') {
+                $this->data['position_start_intron'] = $aPositions[0]->offset;
+                $this->data['position_end_intron'] = $aPositions[1]->offset;
+            }
+            $this->data['range'] = $this->range;
         }
-        $this->data['range'] = $this->range;
 
         // Now, store the corrected value.
         if ($this->matched_pattern == 'uncertain_range') {
