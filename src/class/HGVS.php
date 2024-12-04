8b1b246eb6c375d2564d82ba08ec66900d67c820
Ivo Fokkema <i.f.a.c.fokkema@lumc.nl>
2024-12-04 12:14:33 +0100

    Intronic positions prefixed by 0 were not picked up.
    This is the same issue that I had before with == vs === in PHP.

--- src/class/HGVS.php
+++ src/class/HGVS.php
@@ -1034,7 +1034,7 @@ class HGVS_DNAPosition extends HGVS
             } elseif ($this->intronic && !$this->unknown_offset) {
                 if (!$this->offset) {
                     $this->messages['EPOSITIONFORMAT'] = 'This variant description contains an invalid intronic position: "' . $this->value . '".';
-                } elseif ((string) abs($this->offset) != $this->regex[4]) {
+                } elseif ((string) abs($this->offset) !== $this->regex[4]) {
                     $this->messages['WPOSITIONWITHZERO'] = 'Intronic positions should not be prefixed by a 0.';
                     $nCorrectionConfidence *= 0.9;
                 }
